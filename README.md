bff76bb0bb11dae72927653af6c8be6afb564ee0
Jesús Gómez <jgomo3@gmail.com>
2017-01-17 11:00:08 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+Supose the server is named `troy` or have the IP `192.168.0.2`
+
+Save this folder in the server you want yo upload a file to.
+
+Then, `cd` to that folder and excecute the site with:
+
+    python uploadhere.py
+
+Then, from the machine where your files are, visit the following page:
+
+ * `http://troy:8080`
+ * `http://192.168.0.2:8080`
+ 
+ And upload a file with the form.
+ 
+ The file will be in the folder containing this site.
---
+++ uploadhere.py
@@ -0,0 +1,17 @@
+import os
+from bottle import post, route, run, request
+from bottle import static_file
+
+@route('/')
+def server_static():
+    return static_file('uploader.html', root='.')
+
+@route('/upload', method='POST')
+def do_upload():
+    upload     = request.files.get('upload')
+    name, ext = os.path.splitext(upload.filename)
+    save_path = '.' 
+    upload.save(save_path) # appends upload.filename automatically
+    return 'OK'
+
+run(host='localhost', port=8080)
